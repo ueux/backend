f8e7defbe3135f04407b51ca18155add711824c1
Harsh Kumar <hemenderkumar3000@gmail.com>
2024-08-12 07:19:12 +0530

    login and logout controllers and auth middleware  added

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,9 +17,9 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
         // file has been uploaded successfull
         //console.log("file is uploaded on cloudinary ", response.url);
+        // console.log("CLOUDINARY RESPONSE : ",response)
         fs.unlinkSync(localFilePath)
         return response;
-
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
